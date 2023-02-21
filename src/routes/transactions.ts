8ec83aa5810139573b53d4e9c8cf7d0e4ccfff2a
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2023-02-21 14:35:34 -0300

    feat: create endpoint to create transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,10 +1,30 @@
 import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
+import { z } from 'zod'
+import crypto from 'node:crypto'
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    const select = await knex('transactions').select('*')
+  app.get('/list-transactions', async () => {
+    const listTransactions = await knex('transactions').select('*')
 
-    return select
+    return listTransactions
+  })
+
+  app.post('/', async (request, response) => {
+    const createTrasactionsSchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    })
+
+    const body = createTrasactionsSchema.parse(request.body)
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title: body.title,
+      amount: body.type === 'credit' ? body.amount : body.amount * -1,
+    })
+
+    return response.status(201).send()
   })
 }
