3da0da81165d89b04ff9290a9fddf2a7ebd4f0cc
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2023-02-21 01:39:34 -0300

    chore: create routes

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,10 @@
 import fastify from 'fastify'
-import { knex } from './database'
-import crypto from 'node:crypto'
 import { env } from './env'
+import { transactionRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-  // const transaction = await knex('transactions')
-  //   .insert({
-  //     id: crypto.randomUUID(),
-  //     title: 'Transacão teste',
-  //     amount: 1200,
-  //   })
-  //   .returning('*')
-
-  const select = await knex('transactions').select('*')
-
-  return select
-})
+app.register(transactionRoutes)
 
 app
   .listen({
