dae57463fdbe820f765b5a27c3c93577273b645b
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2023-02-24 23:18:27 -0300

    create middleare to validate if is the same user

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,49 +2,62 @@ import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { z } from 'zod'
 import crypto, { randomUUID } from 'node:crypto'
+import { checkSessionIdExist } from '../middlewares/checkSessionIdExist'
 
 export async function transactionRoutes(app: FastifyInstance) {
-  app.get('/', async (request, reply) => {
-    const sessionId = request.cookies.sessionId
+  app.get(
+    '/',
+    {
+      preHandler: [checkSessionIdExist],
+    },
+    async (request, reply) => {
+      const sessionId = request.cookies.sessionId
 
-    if (!sessionId) {
-      return reply.status(401).send({
-        error: 'Não autorizado',
+      const listTransactions = await knex('transactions')
+        .select()
+        .where('session_id', sessionId)
+
+      return {
+        listTransactions,
+      }
+    },
+  )
+
+  app.get(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExist],
+    },
+    async (request) => {
+      const requestSchema = z.object({
+        id: z.string().uuid(),
       })
-    }
 
-    const listTransactions = await knex('transactions')
-      .select()
-      .where('session_id', sessionId)
+      const { id } = requestSchema.parse(request.params)
 
-    return {
-      listTransactions,
-    }
-  })
+      const findTransaction = await knex('transactions').where('id', id).first()
 
-  app.get('/:id', async (request) => {
-    const requestSchema = z.object({
-      id: z.string().uuid(),
-    })
+      return {
+        findTransaction,
+      }
+    },
+  )
 
-    const { id } = requestSchema.parse(request.params)
+  app.get(
+    '/resume',
+    {
+      preHandler: [checkSessionIdExist],
+    },
+    async () => {
+      const resumeTransactions = await knex('transactions')
+        .sum('amount', { as: 'amount' })
+        .first()
 
-    const findTransaction = await knex('transactions').where('id', id).first()
-
-    return {
-      findTransaction,
-    }
-  })
-
-  app.get('/resume', async () => {
-    const resumeTransactions = await knex('transactions')
-      .sum('amount', { as: 'amount' })
-      .first()
-
-    return {
-      resumeTransactions,
-    }
-  })
+      return {
+        resumeTransactions,
+      }
+    },
+  )
 
   app.post('/', async (request, reply) => {
     // Fastify use reply instead response
